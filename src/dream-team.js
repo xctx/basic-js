5ecf3241e9a230ab76cfad94c98c63f99a129116
xct <xcat182@gmail.com>
2021-03-27 15:58:47 +0300

    feat: filter non-string elements

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,10 +1,18 @@
 
 module.exports = function createDreamTeam(members) {
-  
-  if (members instanceof Error) return false;
+
+  if (members instanceof Error) {
+    return false;
+  }
   
   for (let i=0; i<members.length; i++){
     if (members[i] instanceof Error) return false;
   } 
 
-  };
+  const isString= (el) => {
+    return (typeof el  === 'string');
+  }
+
+  members = members.filter(isString);
+
+};
