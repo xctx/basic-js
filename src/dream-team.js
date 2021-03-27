666816f7ca54f50a908f4503dc9c7f406674e841
xct <xcat182@gmail.com>
2021-03-27 14:49:29 +0300

    feat: handle erroneous elements in array

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  
+  if (members instanceof Error) return false;
+  
+  for (let i=0; i<members.length; i++){
+    if (members[i] instanceof Error) return false;
+  } 
+
+  };
