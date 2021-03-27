309b8200389c0ecdc886d0c166d2069b81e4b24d
xct <xcat182@gmail.com>
2021-03-27 20:11:43 +0300

    feat: handle input data

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,14 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  
+  str = String.toString(str);
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  addition = String.toString(options.addition);
+
+  repeatTimes = (typeof options.repeatTimes === "number") ? options.repeatTimes : 0;
+  additionRepeatTimes = (typeof options.additionRepeatTimes === "number") ? options.additionRepeatTimes : 0;
+
+  separator = (options.separator) ? options.separator : "+";
+  additionSeparator = (options.additionSeparator) ? options.additionSeparator : "|";
+  
 };
   
