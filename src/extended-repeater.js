46bbd7cb11628d8c93d5729634b422ca83dfd2d5
xct <xcat182@gmail.com>
2021-03-28 15:02:08 +0300

    feat: implement the rest functionality

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,14 +1,28 @@
 module.exports = function repeater(str, options) {
-  
-  str = String.toString(str);
 
-  addition = String.toString(options.addition);
+  let addition = ((typeof options.addition === 'string') && 
+                  (options.addition !== null ))? options.addition: ("" + options.addition);
 
-  repeatTimes = (typeof options.repeatTimes === "number") ? options.repeatTimes : 0;
-  additionRepeatTimes = (typeof options.additionRepeatTimes === "number") ? options.additionRepeatTimes : 0;
+  if (typeof options.addition === 'undefined') addition = ''; 
+ 
+  let repeatTimes = (typeof options.repeatTimes !== "number") ? 0 : options.repeatTimes;
+  let additionRepeatTimes = (typeof options.additionRepeatTimes !== "number") ? 0 : options.additionRepeatTimes;
 
-  separator = (options.separator) ? options.separator : "+";
-  additionSeparator = (options.additionSeparator) ? options.additionSeparator : "|";
-  
+  repeatTimes = (typeof options.repeatTimes === 'undefined')? 1 : options.repeatTimes;
+  additionRepeatTimes = (typeof options.additionRepeatTimes === 'undefined') ? 1 : options.additionRepeatTimes;
+
+  let separator = (options.separator) ? options.separator : "+";
+  let additionSeparator = (options.additionSeparator) ? options.additionSeparator : "|";
+  let res='', add='';
+
+  for (let j=0; j<additionRepeatTimes; j++){
+    add = add + addition + (j === additionRepeatTimes-1?'':additionSeparator);
+  }
+
+  for (let i=0; i<repeatTimes; i++){
+    res = res + str + add + (i === repeatTimes-1?'':separator);
+  } 
+
+  return res;
 };
   
